2683a9c99017483b0e48307fdfbe890a280f99b5
gitone912 <akash.101@kvsroranchi.in>
2024-05-08 23:20:58 +0530

    j

--- faculty_management/settings.py
+++ faculty_management/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -139,6 +139,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # In your Django settings file (settings.py)
 LOGIN_URL = '/faculty/login'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+MEDIA_URLS ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
