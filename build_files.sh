d3fad988acd21cfcad806a95e4db30ac44011cb4
gitone912 <akash.101@kvsroranchi.in>
2024-05-08 23:25:45 +0530

    d

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,7 @@
 echo "BUILD START"
- python3.9 -m pip install -r requirements.txt
+ python3.9 -m pip install django
+ python3.9 -m pip install python-docx
+ python3.9 -m pip install openpyxl
+ python3.9 -m pip install psycopg2-binary~=2.9.3
  python3.9 manage.py collectstatic --noinput --clear
  echo "BUILD END"
