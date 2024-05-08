ed9bd06272419930ca5b19b7bd5bca1a377dbaa8
gitone912 <akash.101@kvsroranchi.in>
2024-05-08 23:15:46 +0530

    d

--- faculty_management/settings.py
+++ faculty_management/settings.py
@@ -139,6 +139,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # In your Django settings file (settings.py)
 LOGIN_URL = '/faculty/login'
 
-PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
